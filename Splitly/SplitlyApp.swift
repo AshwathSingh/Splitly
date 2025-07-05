030965e9cfe128183017c384bb084dcbd52b940b
Ashwath <ashwath.singh2005@gmail.com>
2025-07-06 00:43:48 +0530

    Initial Commit

---
+++ Splitly/SplitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitlyApp.swift
+//  Splitly
+//
+//  Created by Ashwath Singh on 06/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SplitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SplitlyTests/SplitlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitlyTests.swift
+//  SplitlyTests
+//
+//  Created by Ashwath Singh on 06/07/25.
+//
+
+import Testing
+@testable import Splitly
+
+struct SplitlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
